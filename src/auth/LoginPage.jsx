a8dba1de90bab51d029f59ff686479bbac5e0c91
Project12df <eflores@powersathletic.com>
2025-05-15 13:26:31 -0600

    Login-css New

--- src/auth/LoginPage.jsx
+++ src/auth/LoginPage.jsx
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-
-
 import "./LoginPageStyle.css";
+import logo from '../assets/logpms.png';
+
+
+
 const LoginPage = () => {
     const { login } = useAuth();
     const navigate = useNavigate();
@@ -13,33 +15,28 @@ const LoginPage = () => {
     };
 
     return (
-        <div className="wrapper">
-            <div className="container">
-                <div className="col-left">
-                    <div className="login-text">
-                        <h2>Bienvenido</h2>
-                        <h2>Logo</h2>
-                        
+        <div className="wrapper" >
+           <div className="forms-container">
+                <div className="signin-signup">
+                    <div className="header-container">
+                        <img src={logo} alt="Logo Powers Athletics" className="form-logo" /> 
+                        <h2 className="form-title">PMS LOGIN</h2>
                     </div>
-                </div>
-                <div className="col-right">
-                    <div className="login-form">
-                        <h2>Login</h2>
-                        <form>
-                            <p>
-                                <input type="text" placeholder="Username" />
-                            </p>
-                            <p>
-                                <input type="password" placeholder="Password" />
-                            </p>
-                            <p>
-                                <input className="btn" onClick={handleLogin} value="Iniciar Sesión" />
-                            </p>
+                        <form className="sign-in-form" id="login-form">
+                            <div className="input-field">
+                                <label>Username<span className="required">*</span></label>
+                                <input className="inp1" type="text" placeholder="Username" />
+                            </div>
+                            <div className="input-field">
+                                <label>Password<span className="required">*</span></label>
+                                <input className="inp1" type="password" placeholder="Password" />
+                            </div>
+                            <div className="input-field">
+                                <input className="btn" type="button" onClick={handleLogin} value="LOGIN" />
+                            </div>
                         </form>
-                    </div>
                 </div>
             </div>
-
         </div>
     );
 };
